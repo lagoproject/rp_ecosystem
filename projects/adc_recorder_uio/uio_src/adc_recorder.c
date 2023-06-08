9a2e59c9cc87159c7eee07fa27197f29a88b5e50
Horacio Arnaldi <lharnaldi@gmail.com>
2023-06-08 07:41:12 -0300

    Agrego volatile al cma y comento algunas salidas no necesarias en adc_recorder_uio

--- projects/adc_recorder_uio/uio_src/adc_recorder.c
+++ projects/adc_recorder_uio/uio_src/adc_recorder.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	uint32_t wo;
 	int16_t ch[2];
 
-	printf("ADC RECORDER test\n");
+	//printf("ADC RECORDER test\n");
 
 	//initialize devices. TODO: add error checking 
 	//intc_init();
@@ -20,45 +20,45 @@ int main(int argc, char *argv[])
 	// set writer address
 	//   *(uint32_t *)(cfg + 4) = size;
 	//
-	printf("Set writer address...\n");
-	val=rd_reg_value(1, CFG_WR_ADDR_OFFSET);
-	printf("dev_size es: %d ...\n",dev_size);
-	wr_reg_value(1, CFG_WR_ADDR_OFFSET, dev_size);
+	//printf("Set writer address...\n");
+	val=rd_reg_value(1, CFG_WR_ADDR_OFFSET,0);
+	//printf("dev_size es: %d ...\n",dev_size);
+	wr_reg_value(1, CFG_WR_ADDR_OFFSET, dev_size,0);
 
 	 // set number of samples
 	 //   *(uint32_t *)(cfg + 8) = 1024 * 1024 - 1;
 	 //
-	printf("Set number of samples...\n");
-	val=rd_reg_value(1, CFG_NSAMPLES_OFFSET);
-	wr_reg_value(1, CFG_NSAMPLES_OFFSET, 1024 * 1024 - 1);
+	//printf("Set number of samples...\n");
+	val=rd_reg_value(1, CFG_NSAMPLES_OFFSET,0);
+	wr_reg_value(1, CFG_NSAMPLES_OFFSET, 1024 * 1024 - 1,0);
 
 	// reset writer
 	//*((uint32_t *)(cfg + 0)) &= ~4;
-	printf("Reseting writer...\n");
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &= ~4);
+	//printf("Reseting writer...\n");
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &= ~4,0);
 	//*((uint32_t *)(cfg + 0)) |= 4;
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |= 4);
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |= 4,0);
 //	printf("Reseting writer %d ...\n",val);
-	printf("Reseting fifo and filters...\n");
+	//printf("Reseting fifo and filters...\n");
 //	// reset fifo and filters
 	//*((uint32_t *)(cfg + 0)) &= ~1;
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &=~1);
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &=~1,0);
 //	//*((uint32_t *)(cfg + 0)) |= 1;
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |=1);
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |=1,0);
 //	printf("Reseting fifo and filters %d ...\n",val);
 //
 //	// wait 1 second
 	sleep(1);
 //
-	printf("Reseting packetizer...\n");
+	//printf("Reseting packetizer...\n");
 	// enter reset mode for packetizer
 	//*((uint32_t *)(cfg + 0)) &= ~2; 
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &=~2);
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val &=~2,0);
 
 	// set number of samples
 	//*((uint32_t *)(cfg + 4)) = 1024 * 1024 - 1;
@@ -66,8 +66,8 @@ int main(int argc, char *argv[])
 
 	// enter normal mode
 //	//*((uint32_t *)(cfg + 0)) |= 2;
-	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET);
-	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |=2);
+	val=rd_reg_value(1, CFG_RESET_GRAL_OFFSET,0);
+	wr_reg_value(1, CFG_RESET_GRAL_OFFSET, val |=2,0);
 //	printf("Reseting packetizer %d ...\n",val);
 //
 //	// wait 1 second
@@ -75,9 +75,11 @@ int main(int argc, char *argv[])
 
 	// print IN1 and IN2 samples
 	for(i = 0; i < 1024 * 1024; ++i){
-		ch[0] = *((int16_t *)(cma_ptr + 2*i + 0));
-		ch[1] = *((int16_t *)(cma_ptr + 2*i + 1));
-		wo = *((uint32_t *)(cma_ptr + i));
+		ch[0] = cma_ptr[2*i + 0];
+		ch[1] = cma_ptr[2*i + 1];
+		//ch[0] = *((int16_t *)(cma_ptr + 2*i + 0));
+		//ch[1] = *((int16_t *)(cma_ptr + 2*i + 1));
+		wo = *((uint32_t *)(cma_ptr + 2*i));
 		printf("%5d %5d %10d\n", ch[0], ch[1], wo);
 //		ch[0] = cma_ptr[2 * i + 0];
 //		ch[1] = cma_ptr[2 * i + 1];
@@ -96,7 +98,7 @@ int main(int argc, char *argv[])
 	close(cfg_fd);
 	//close(sts_fd);
 	//close(xadc_fd);
-	printf("Saliendo ...\n");
+	//printf("Saliendo ...\n");
 
 	return 0;
 
